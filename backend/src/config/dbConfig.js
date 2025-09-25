7e297497312f31aac21fa1b2ddb91fee6aca8882
Praveen Kumar <praveenshakya441@gmail.com>
2025-09-25 18:44:04 +0530

    Added Basic Routes and modify dbConnect

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import { NODE_ENV } from "../config/serverConfig.js";
-import { DEV_DB_URL, PROD_DB_URL} from "../config/serverConfig.js";
+import mongoose from 'mongoose';
 
-export default async function  connectDB() {
-    try {
-        if(NODE_ENV !== "development") {
-            await mongoose.connect(DEV_DB_URL)
-        }
-        else if(NODE_ENV === 'production') {
-            await mongoose.connect(PROD_DB_URL)
-        }
+import { NODE_ENV } from '../config/serverConfig.js';
+import { DEV_DB_URL, PROD_DB_URL } from '../config/serverConfig.js';
 
-        console.log(`Connected to database from ${NODE_ENV} environment`);
-    } catch (error) {
-        console.log("Error connecting to database", error)
+export default async function connectDB() {
+  try {
+    if (NODE_ENV !== 'development') {
+      await mongoose.connect(DEV_DB_URL);
+    } else if (NODE_ENV === 'production') {
+      await mongoose.connect(PROD_DB_URL);
     }
+
+    console.log(`Connected to database from ${NODE_ENV} environment`);
+  } catch (error) {
+    console.log('Error connecting to database', error);
+  }
 }
